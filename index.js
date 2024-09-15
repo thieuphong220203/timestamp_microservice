4d47f1c36bd8e4b37982878bd880c9781d418a86
Phong <tangthieuphong888@gmail.com>
2024-09-16 01:04:41 +0700

    first commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
